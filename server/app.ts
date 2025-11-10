415f2f4c4f9e25605c8e343d3e4c3d7358e1d6c9
Trần Gia Bảo <trangiabao02032003@gmail.com>
2025-11-10 21:46:52 +0700

    Database, Cloudinary, Redis connection

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,35 @@
-import express from "express";
+require("dotenv").config();
+
+import express, { NextFunction, Request, Response } from "express";
 export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+//body parser
+app.use(express.json({ limit: "50mb" }));
+
+//cookie parser
+app.use(cookieParser());
+
+//cors => cross origin resource sharing
+app.use(
+	cors({
+		origin: process.env.ORIGIN,
+		credentials: true,
+	})
+);
+
+//testing api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+	res.status(200).json({
+		success: true,
+		message: "API is working fine",
+	});
+});
+
+//unknown route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+	const err = new Error(`Route ${req.originalUrl} not found`) as any;
+	err.statusCode = 404;
+	next(err);
+});
