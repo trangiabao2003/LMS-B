6bb734eba6c76d8ea1d0df8a96650816f0ba43e7
Trần Gia Bảo <trangiabao02032003@gmail.com>
2025-11-14 22:10:06 +0700

    User registration

--- server/app.ts
+++ server/app.ts
@@ -5,6 +5,7 @@ export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
+import userRouter from "./routes/user.route";
 
 //body parser
 app.use(express.json({ limit: "50mb" }));
@@ -19,6 +20,9 @@ app.use(
 		credentials: true,
 	})
 );
+
+//routes
+app.use("/api/v1", userRouter);
 
 //testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
