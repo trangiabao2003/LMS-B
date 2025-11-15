6b6e6eb2fd39cb3f8d12d4350345750a50de731a
Trần Gia Bảo <trangiabao02032003@gmail.com>
2025-11-15 14:38:38 +0700

    User protected & Authorization middleware

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -5,6 +5,7 @@ import {
 	logoutUser,
 	registrationUser,
 } from "../controllers/user.controller";
+import { isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
 
 userRouter.post("/registration", registrationUser);
@@ -13,6 +14,6 @@ userRouter.post("/activate-user", activateUser);
 
 userRouter.post("/login", loginUser);
 
-userRouter.get("/logout", logoutUser);
+userRouter.get("/logout", isAuthenticated, logoutUser);
 
 export default userRouter;
