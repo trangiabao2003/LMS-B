45ccb55d0c5f3a4e42a9959c0da6bad0a2fa51e6
Trần Gia Bảo <trangiabao02032003@gmail.com>
2025-11-16 08:58:28 +0700

    Update user info, password, avatar

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -7,6 +7,9 @@ import {
 	registrationUser,
 	socialAuth,
 	updateAccessToken,
+	updatePassword,
+	updateProfilePicture,
+	updateUserInfo,
 } from "../controllers/user.controller";
 import { isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
@@ -24,5 +27,11 @@ userRouter.get("/refresh", updateAccessToken);
 userRouter.get("/me", isAuthenticated, getUserInfo);
 
 userRouter.post("/social-auth", socialAuth);
+
+userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
+
+userRouter.put("/update-user-password", isAuthenticated, updatePassword);
+
+userRouter.put("/update-user-avatar", isAuthenticated, updateProfilePicture);
 
 export default userRouter;
