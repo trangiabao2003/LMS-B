f2ce4562f4f08c8aeb75ef1593bd14c4173901e9
Trần Gia Bảo <trangiabao02032003@gmail.com>
2025-11-17 21:25:15 +0700

    Create Order

---
+++ server/services/order.service.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Response } from "express";
+import { CatchAsyncErrors } from "../middleware/catchAsyncErrors";
+import OrderModel from "../models/order.model";
+
+//create new order
+export const newOrder = CatchAsyncErrors(async (data: any, res: Response) => {
+	const order = await OrderModel.create(data);
+	res.status(201).json({
+		success: true,
+		order,
+	});
+});
