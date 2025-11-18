d2e77e90cb775a8fd91a2ed66ee418ec3c26c1ef
Trần Gia Bảo <trangiabao02032003@gmail.com>
2025-11-18 21:32:17 +0700

    Advance cache maintenance

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -87,7 +87,7 @@ export const getSingleCourse = CatchAsyncErrors(
 					"-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
 				);
 
-				await redis.set(courseId, JSON.stringify(course));
+				await redis.set(courseId, JSON.stringify(course), "EX", 604800);
 				res.status(200).json({
 					success: true,
 					course,
--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -190,7 +190,9 @@ export const updateAccessToken = CatchAsyncErrors(
 			}
 			const session = await redis.get(decoded.id as string);
 			if (!session) {
-				return next(new ErrorHandler(message, 400));
+				return next(
+					new ErrorHandler("Please login for access this resources", 400)
+				);
 			}
 			const user = JSON.parse(session);
 
@@ -210,6 +212,8 @@ export const updateAccessToken = CatchAsyncErrors(
 
 			res.cookie("access_token", accessToken, accessTokenOptions);
 			res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+
+			await redis.set(user._id, JSON.stringify(user), "EX", 604800); // 7days
 
 			res.status(200).json({
 				status: "success",
