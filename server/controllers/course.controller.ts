6e97d593ca6668d69f0304614f7b68fa24077a69
Trần Gia Bảo <trangiabao02032003@gmail.com>
2025-11-18 09:50:41 +0700

    Delete course, delete user

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -398,3 +398,25 @@ export const getAllCoursess = CatchAsyncErrors(
 		}
 	}
 );
+
+//delete course -- only for admin
+export const deleteCourse = CatchAsyncErrors(
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const { id } = req.params;
+			const course = await CourseModel.findById(id);
+			if (!course) {
+				return next(new ErrorHandler("Course not found", 404));
+			}
+
+			await course.deleteOne({ id });
+			await redis.del(id);
+			res.status(200).json({
+				success: true,
+				message: "Course deleted successfully",
+			});
+		} catch (error: any) {
+			return next(new ErrorHandler(error.message, 400));
+		}
+	}
+);
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -4,6 +4,7 @@ import {
 	addQuestion,
 	addReplyToReview,
 	addReview,
+	deleteCourse,
 	editCourse,
 	getAllCourses,
 	getAllCoursess,
@@ -52,6 +53,13 @@ courseRouter.get(
 	isAuthenticated,
 	authorizeRoles("admin"),
 	getAllCoursess
+);
+
+courseRouter.delete(
+	"/delete-course/:id",
+	isAuthenticated,
+	authorizeRoles("admin"),
+	deleteCourse
 );
 
 export default courseRouter;
